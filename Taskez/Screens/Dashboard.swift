0475ecbc78edfd2e3cae7e1914b8e64123b3924c
Oleksandr <oleksandr@MacBook-Pro-Oleksandr.local>
2023-06-30 18:45:44 +0300

    add background component

--- Taskez/Screens/Dashboard.swift
+++ Taskez/Screens/Dashboard.swift
@@ -8,8 +8,60 @@
 import SwiftUI
 
 struct Dashboard: View {
+    @State private var selectedTab: Int = 0
     var body: some View {
-        BacgroundView()
+        NavigationStack {
+            ZStack {
+                BacgroundView()
+                
+                
+                VStack () {
+                    HStack {
+                        Text("Hello, \nDerek Doyle 👋")
+                            .padding(.top, 22)
+                            .foregroundColor(.white)
+                        Spacer()
+                    }
+                    
+                    Picker("", selection: $selectedTab) {
+                        Image("chatIcon").tag(0)
+                                    Text("Second").tag(1)
+                                    Text("Third").tag(2)
+                                }
+                                .pickerStyle(SegmentedPickerStyle())
+                                
+                    
+//                    List {
+//                        NavigationLink("Hello") {
+//                            Text("Hello")
+//                        }
+//
+//                    }
+//                    .scrollContentBackground(.hidden)
+//                    .padding(24)
+                    Spacer()
+                }
+            }
+            .navigationBarTitleDisplayMode(.inline)
+            .toolbar {
+                ToolbarItem(placement: .principal) {
+                    HStack {
+                        Text("Dashboard")
+                            .foregroundColor(.white)
+                        Spacer()
+                        HStack ( spacing: 34) {
+                            Image("chatIcon")
+                                .frame(width: 24, height: 24)
+                            Circle()
+                                .fill(Color.red)
+                                .frame(width: 48)
+                            
+                        }
+                    }
+                    .padding(.horizontal, 24)
+                }
+            }
+        }
     }
 }
 
